2e98d81cba46f05a645b86025d9700018778e506
2015 MacBook <2015macbook@MacBook-Air.local>
2021-11-19 20:49:49 +0900

    make it better

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('rJx7ygh+drEY5jR7wTnXSc3eF0ZIvEJzjTDT7qo0/xoj6IxOpFLC5DL9Q9BFBNNbUmbk92/j0ulF/v5rubGerfv5YJVhdkMjp4OuesInGAN8iPz/LIeGvdfU70QHg73whwCv0uYVa31Jz5m0tpqQbgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('4618cba20c09232e59c0a554dab4c645')
 
 @app.route("/")
-def test():
-    return "OK"
-    
+def hello_world():
+    return "hallo world"
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
